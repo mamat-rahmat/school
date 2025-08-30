48b1bd21804ab32daa988a7bef1bc10def69edf5
Mamat Rahmat <coderbodoh@gmail.com>
2025-08-30 09:25:32 +0700

    Update activity page

--- resources/views/activities.blade.php
+++ resources/views/activities.blade.php
@@ -2,71 +2,61 @@
 
 @section('content')
 <div class="container py-5">
-    <h1 class="text-center mb-5">Aktivitas Sekolah</h1>
+    <h1 class="text-center mb-5 fw-bold">Aktivitas Sekolah</h1>
+
+    @php
+    $activities = [
+        [
+            'image' => 'academic.jpeg',
+            'alt' => 'Academic Activities',
+            'title' => 'Program Akademik Unggulan',
+            'description' => 'Program akademik khusus kami membantu siswa mencapai potensi penuh mereka melalui kesempatan belajar tingkat lanjut dan bimbingan yang dipersonalisasi.'
+        ],
+        [
+            'image' => 'sports.jpg',
+            'alt' => 'Sports Activities',
+            'title' => 'Program Pengembangan Olahraga',
+            'description' => 'Program olahraga komprehensif meliputi sepak bola, bola basket, renang, dan atletik untuk mengembangkan kebugaran fisik dan semangat tim.'
+        ],
+        [
+            'image' => 'arts.jpeg',
+            'alt' => 'Arts Activities',
+            'title' => 'Program Seni & Budaya',
+            'description' => 'Pameran seni, pertunjukan musik, dan festival budaya rutin untuk mengembangkan kreativitas dan ekspresi artistik.'
+        ],
+        [
+            'image' => 'organizations.jpeg',
+            'alt' => 'Student Organizations',
+            'title' => 'Organisasi Siswa',
+            'description' => 'Berbagai klub dan organisasi siswa yang mengembangkan jiwa kepemimpinan, kerja sama tim, dan tanggung jawab sosial.'
+        ],
+        [
+            'image' => 'community.jpg',
+            'alt' => 'Community Service',
+            'title' => 'Program Pengabdian Masyarakat',
+            'description' => 'Kegiatan pelayanan masyarakat rutin yang membantu siswa mengembangkan rasa empati dan kesadaran sosial.'
+        ],
+        [
+            'image' => 'events.jpeg',
+            'alt' => 'Special Events',
+            'title' => 'Acara Khusus',
+            'description' => 'Festival sekolah tahunan, pameran sains, dan perayaan khusus yang menyatukan komunitas sekolah kami.'
+        ]
+    ];
+    @endphp
 
     <div class="row g-4">
+        @foreach($activities as $activity)
         <div class="col-md-4">
-            <div class="card h-100 shadow-sm">
-                <img src="{{ asset('images/academic.jpeg') }}" class="card-img-top activity-img" alt="Academic Activities">
+            <div class="card h-100 shadow-sm hover-shadow">
+                <img src="{{ asset('images/' . $activity['image']) }}" class="card-img-top activity-img" alt="{{ $activity['alt'] }}">
                 <div class="card-body">
-                    <h5 class="card-title">Program Akademik Unggulan</h5>
-                    <p class="card-text">Program akademik khusus kami membantu siswa mencapai potensi penuh mereka melalui kesempatan belajar tingkat lanjut dan bimbingan yang dipersonalisasi.</p>
+                    <h5 class="card-title">{{ $activity['title'] }}</h5>
+                    <p class="card-text">{{ $activity['description'] }}</p>
                 </div>
             </div>
         </div>
-
-        <div class="col-md-4">
-            <div class="card h-100 shadow-sm">
-                <img src="{{ asset('images/sports.jpg') }}" class="card-img-top activity-img" alt="Sports Activities">
-                <div class="card-body">
-                    <h5 class="card-title">Program Pengembangan Olahraga</h5>
-                    <p class="card-text">Program olahraga komprehensif meliputi sepak bola, bola basket, renang, dan atletik untuk mengembangkan kebugaran fisik dan semangat tim.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card h-100 shadow-sm">
-                <img src="{{ asset('images/arts.jpeg') }}" class="card-img-top activity-img" alt="Arts Activities">
-                <div class="card-body">
-                    <h5 class="card-title">Program Seni & Budaya</h5>
-                    <p class="card-text">Pameran seni, pertunjukan musik, dan festival budaya rutin untuk mengembangkan kreativitas dan ekspresi artistik.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card h-100 shadow-sm">
-                <img src="{{ asset('images/organizations.jpeg') }}" class="card-img-top activity-img" alt="Student Organizations">
-
-                <div class="card-body">
-                    <h5 class="card-title">Organisasi Siswa</h5>
-                    <p class="card-text">Berbagai klub dan organisasi siswa yang mengembangkan jiwa kepemimpinan, kerja sama tim, dan tanggung jawab sosial.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card h-100 shadow-sm">
-                <img src="{{ asset('images/community.jpg') }}" class="card-img-top activity-img" alt="Community Service">
-
-                <div class="card-body">
-                    <h5 class="card-title">Program Pengabdian Masyarakat</h5>
-                    <p class="card-text">Kegiatan pelayanan masyarakat rutin yang membantu siswa mengembangkan rasa empati dan kesadaran sosial.</p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col-md-4">
-            <div class="card h-100 shadow-sm">
-                <img src="{{ asset('images/events.jpeg') }}" class="card-img-top activity-img" alt="Special Events">
-
-                <div class="card-body">
-                    <h5 class="card-title">Acara Khusus</h5>
-                    <p class="card-text">Festival sekolah tahunan, pameran sains, dan perayaan khusus yang menyatukan komunitas sekolah kami.</p>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 </div>
 
